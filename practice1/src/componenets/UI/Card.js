cdded0015e88a659672f9ab04b8e10b27aaf663d
smforuzmand <career.forouzmand@gmail.com>
2023-01-09 23:56:15 +0100

    Adding the AddUser style

--- practice1/src/componenets/UI/Card.js
+++ practice1/src/componenets/UI/Card.js
@@ -4,7 +4,7 @@ const Card = (props) => {
     return (
 
 
-        <div className='card'>
+        <div className={classes.card}>
 
             {props.children}
 
--- practice1/src/componenets/Users/AddUser.js
+++ practice1/src/componenets/Users/AddUser.js
@@ -1,5 +1,8 @@
 import React from 'react';
-import Card from '../UI/Card';
+import Card from '../UI/Card'
+import classes from './AddUser.css'
+
+
 const AddUser = (props) => {
 
     const addUserHandler = (event) => {
@@ -14,7 +17,7 @@ const AddUser = (props) => {
 
 
     return (
-        <Card>
+        <Card className={classes.input}>
 
             <form onSubmit={addUserHandler} >
                 <label htmlFor="username">Username</label>
