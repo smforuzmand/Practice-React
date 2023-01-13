60eae5782e19cc079ab9a6b6f3753706d2e60a77
smforuzmand <career.forouzmand@gmail.com>
2023-01-13 22:07:42 +0100

    adding reseting and basic validation to the form

--- practice1/src/componenets/Users/AddUser.js
+++ practice1/src/componenets/Users/AddUser.js
@@ -12,10 +12,21 @@ const AddUser = (props) => {
     const addUserHandler = (event) => {
 
         event.preventDefault();
+        if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+            return;
+
+        }
+
+        if (+enteredAge < 1) {
+            return
+
+        }
+
+
 
         console.log(enteredUsername, enteredAge)
-
-
+        setEnteredUsername(' ');
+        setEnteredAge('')
     };
 
     const usernameChangeHandler = (event) => {
@@ -30,9 +41,9 @@ const AddUser = (props) => {
 
             <form onSubmit={addUserHandler} >
                 <label htmlFor="username" >Username</label>
-                <input id='username' type="text" onChange={usernameChangeHandler} />
+                <input id='username' type="text" onChange={usernameChangeHandler} value={enteredUsername} />
                 <label htmlFor="age">Age (Years)</label>
-                <input id='age' type="number" onChange={ageChangeHandler} />
+                <input id='age' type="number" onChange={ageChangeHandler} value={enteredAge} />
                 <Button type='submit'>Add User</Button>
             </form>
 
