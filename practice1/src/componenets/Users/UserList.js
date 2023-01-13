76b5c9bc676aa84e999d89b8e137ae699e335660
smforuzmand <career.forouzmand@gmail.com>
2023-01-13 22:53:55 +0100

    Adding card to forms as the styler

--- practice1/src/componenets/Users/UserList.js
+++ practice1/src/componenets/Users/UserList.js
@@ -1,21 +1,23 @@
 //this fucntional component is responsible to outputting the user data
 
 import React from 'react';
+import Card from '../UI/Card';
 
 const UserList = (props) => {
     return (
+        <Card>
+            <ul>
 
-        <ul>
+                {props.users.map((user) => {
 
-            {props.users.map((user) => {
-
-                <li> {user.name} {user.age}</li>
-
-
-            })}
+                    <li> {user.name} {user.age}</li>
 
 
-        </ul>
+                })}
+
+
+            </ul>
+        </Card>
     );
 };
 
