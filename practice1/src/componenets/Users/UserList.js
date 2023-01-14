8c49bfb23e048762d57f27c55ff89e75ea766ced
smforuzmand <career.forouzmand@gmail.com>
2023-01-14 18:01:02 +0100

    Adding userList.module style to the card

--- practice1/src/componenets/Users/UserList.js
+++ practice1/src/componenets/Users/UserList.js
@@ -2,15 +2,17 @@
 
 import React from 'react';
 import Card from '../UI/Card';
+import classes from './UsersList.module.css'
 
 const UserList = (props) => {
     return (
-        <Card>
+        //we add this blow code to not only add the card class but also to have the users style
+        <Card className={classes.users}>
             <ul>
 
                 {props.users.map((user) => {
 
-                    <li> {user.name} {user.age}</li>
+                    <li> {user.name} ({user.age} Years old)</li>
 
 
                 })}
